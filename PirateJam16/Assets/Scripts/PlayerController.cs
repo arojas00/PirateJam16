e92cc6d40677d5367ced94f5df1617eb909c4671
Adrián Rojas <adrian.rojas@sunnaentertainment.com>
2025-01-13 20:08:15 -0600

    tutorial

---
+++ PirateJam16/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private GameObject floor;
+    [SerializeField] private float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
